83e8bb5fa992b4f630fb9d38ae191edb482f730a
Emily Crook <Emily.A.Crook96@gmail.com>
2021-04-24 21:51:40 -0500

    inquirer downloaded

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
 // the 2 grabs the third item in the array after the node app...
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// const pageHTML = generatePage(name, github);
 
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
 // deconstructed version of the above code ^^^
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
 
 // const printProfileData = profileDataArr => {
 //     // This...
@@ -36,8 +51,14 @@ const [name, github] = profileDataArgs;
 // console.log(name, github);
 // console.log(generatePage(name, github));
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Checkout index.html to see the output!');
-})
+//     console.log('Portfolio complete! Checkout index.html to see the output!');
+// })
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Checkout index.html to see the output!');
+// });
