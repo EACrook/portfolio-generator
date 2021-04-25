b466cc9a1bc750c999fe3f47b212f1d4ac191fc6
Emily Crook <Emily.A.Crook96@gmail.com>
2021-04-24 21:02:05 -0500

    generates an HTML page and inputs user info for name and github account

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+// the 2 grabs the third item in the array after the node app...
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+// deconstructed version of the above code ^^^
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
+//     }
+  
+//     console.log('================');
+  
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+//   };
+
+// printProfileData(profileDataArgs);
+
+// Below code allows for just name and github name
+// const generatePage = (userName, githubName) => {
+//     return `
+//     Name: ${userName} 
+//     Github: ${githubName}
+//     `;
+// };
+
+
+
+// ---log the name and github inputs and confirm they match
+// console.log(name, github);
+// console.log(generatePage(name, github));
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Checkout index.html to see the output!');
+})
